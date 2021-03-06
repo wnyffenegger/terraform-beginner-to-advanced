0266a47c0f12ac7c3d187714e1fe345917a154db
wnyffenegger <william.nyffenegger@gmail.com>
2021-03-06 17:14:55 -0600

    remove tstate take 2

--- sandbox/section1/first_ec2.tf
+++ sandbox/section1/first_ec2.tf
@@ -1,19 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-    github = {
-      source = "integrations/github"
-      version = "4.5.1"
-    }
-  }
-}
+# Only one set of required providers in a dir
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#     github = {
+#       source = "integrations/github"
+#       version = "4.5.1"
+#     }
+#   }
+# }
 
 provider "aws" {
     region = "us-east-2"
@@ -24,8 +25,4 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami = "ami-09246ddb00c7c4fef"
     instance_type = "t2.micro"
-}
-
-provider "azurerm" {
-    features {}
 }
--- sandbox/section1/github.tf
+++ sandbox/section1/github.tf
@@ -1,16 +1,16 @@
 # Can only have one required provider
-# terraform {
-#   required_providers {
-#     github = {
-#       source = "integrations/github"
-#       version = "4.5.1"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.5.1"
+    }
+  }
+}
 
 provider "github" {
   # Configuration options
-  token = "fake-token-here"
+  token = "131f55ce52cfd92d67b2e8c5ead8988add9873d0"
 }
 
 resource "github_repository" "terraform-course-example" {
