5fa9277afcd5fa7e2c6baad03f051f6974d6d1b7
wnyffenegger <william.nyffenegger@gmail.com>
2021-03-06 16:29:26 -0600

    remove state and token

--- sandbox/section1/github.tf
+++ sandbox/section1/github.tf
@@ -10,7 +10,7 @@
 
 provider "github" {
   # Configuration options
-  token = "131f55ce52cfd92d67b2e8c5ead8988add9873d0"
+  token = "fake-token-here"
 }
 
 resource "github_repository" "terraform-course-example" {
